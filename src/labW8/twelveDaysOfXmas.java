9ee8346f34fb2f36d19a581e87c37268f039ae64
Liam Junkermann <developer@ljsoftwaresolutions.xyz>
2019-11-01 12:53:02 +0000

    Week 8 Code - Comments added

--- src/labW8/twelveDaysOfXmas.java
+++ src/labW8/twelveDaysOfXmas.java
@@ -1,5 +1,36 @@
 package labW8;
 
+/* SELF ASSESSMENT
+   1. Did I use appropriate CONSTANTS instead of numbers within the code?
+       Mark out of 5: 5
+       Comment: All numbers and frequently used words are listed as constants
+   2. Did I use easy-to-understand, meaningful CONSTANT names formatted correctly in UPPERCASE?
+       Mark out of 5: 5
+       Comment: Each constant name is properly formatted and shows what the constant is used for
+   3. Did I use easy-to-understand meaningful variable names formatted properly (in lowerCamelCase)?
+       Mark out of 10: 10
+       COmment: All variables are properly formatted and the variable names are descriptive
+   4. Did I indent the code appropriately?
+       Mark out of 10: 10
+       Comment: Code is autoformatted by the IDE
+   5. Did I use an appropriate loop (or loops) to produce the different verses?
+       Mark out of 20: 20
+       Comment: Used a for loop to iterate through days, this is the correct loop for this scenario.
+   6. Did I use a switch to build up the verses?
+       Mark out of 25: 25
+       Comment: A switch statment is used to add the correct verse for the day.
+   7. Did I avoid duplication of code and of the lines which make up the verses (each line should be referred to in the code only once (or twice))?
+       Mark out of 10: 10
+       Comment: The verses are built up through each loop, as such each verse is only written once
+   8. Does the program produce the correct output?
+       Mark out of 10: 10
+       Comment: The Program prints the correct output to the console.
+   9. How well did I complete this self-assessment?
+       Mark out of 5: 5
+       Comment: All marks and comments filled out correctly
+   Total Mark out of 100 (Add all the previous marks): 100
+*/
+
 public class twelveDaysOfXmas {
     private static int DAYS_OF_XMAS = 12;
     private static String[] DAY_WORDS = {"first","second","third","fourth","fifth","sixth","seventh","eighth","ninth","tenth","eleventh","twelfth"};
